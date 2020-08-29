645a3ad3606cdb13b9e732f0266d6c92a49b37bc
Sandiegoite <sandiegoite@programmer.net>
2020-08-29 12:04:54 -0700

    Add a game list to the homepage, and an example game object.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+import uuid
+
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
+
+class Game:
+    def __init__(self):
+        self.id = uuid.uuid4()
+    def serialize(self):
+        return {"id": self.id}
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/games')
+def games():
+    return jsonify([Game().serialize() for i in range(10)])
